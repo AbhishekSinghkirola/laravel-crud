b6dc8fd7fec6ba96b03b974fd15cffe46a7d9054
AbhishekSinghkirola <studyabhi82662@gmail.com>
2023-05-30 22:38:07 +0530

    Showing Records

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,22 +24,23 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>
-                                <img src="https://picsum.photos/200" alt="" width="80px">
-                            </td>
-                            <td>Lorem ipsum dolor sit amet.</td>
-                            <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur facere quia molestias
-                                asperiores vitae non iste quae illum,.</td>
-                            <td>News</td>
-                            <td>2-5-23</td>
-                            <td>
-                                <a href="#" class="btn btn-sm btn-success">Show</a>
-                                <a href="#" class="btn btn-sm btn-primary">Edit</a>
-                                <a href="#" class="btn btn-sm btn-danger">Delete</a>
-                            </td>
-                        </tr>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>
+                                    <img src="{{ asset('storage/    ' . $post->image) }}" alt="" width="80px">
+                                </td>
+                                <td>{{ $post->title }}</td>
+                                <td>{{ $post->description }}</td>
+                                <td>{{ $post->category_id }}</td>
+                                <td>{{ $post->created_at }}</td>
+                                <td>
+                                    <a href="#" class="btn btn-sm btn-success">Show</a>
+                                    <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                                    <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
