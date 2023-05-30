b93239ea2f87d01fad813779cca3ffe713dd70dc
AbhishekSinghkirola <studyabhi82662@gmail.com>
2023-05-30 18:20:57 +0530

    Doind Form Validation

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.master')
 @section('content')
     <div class="container">
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div class="alert alert-danger">{{ $error }}</div>
+            @endforeach
+        @endif
         <div class="card">
             <div class="card-header d-flex justify-content-between">
                 <h4> Create Posts</h4>
@@ -9,26 +14,28 @@
                 </div>
             </div>
             <div class="card-body">
-                <form action="" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
                     <div class="form-group">
                         <label for="" class="form-label">Image</label>
-                        <input type="file" class="form-control">
+                        <input type="file" class="form-control" name="image">
                     </div>
                     <div class="form-group">
                         <label for="" class="form-label">Title</label>
-                        <input type="text" class="form-control">
+                        <input type="text" class="form-control" name="title">
                     </div>
                     <div class="form-group">
                         <label for="" class="form-label">Category</label>
-                        <select name="" id="" class="form-control">
-                            <option value="">Test 1</option>
-                            <option value="">Test 2</option>
-                            <option value="">Test 3</option>
+                        <select id="" class="form-control" name="category_id">
+                            <option value="">Select category</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
                         </select>
                     </div>
                     <div class="form-group">
                         <label for="" class="form-label">Description</label>
-                        <textarea name="" id="" class="form-control" cols="30" rows="6"></textarea>
+                        <textarea name="description" id="" class="form-control" cols="30" rows="6"></textarea>
                     </div>
                     <div class="form-group">
                         <button type="submit" class="btn btn-primary">Submit</button>
