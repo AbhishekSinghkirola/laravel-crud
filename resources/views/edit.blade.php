59882455163f0623b35bdbc3d5ae169c8b33e1e1
AbhishekSinghkirola <studyabhi82662@gmail.com>
2023-05-31 16:53:14 +0530

    creating update post functionality

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.master')
 @section('content')
     <div class="container">
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div class="alert alert-danger">{{ $error }}</div>
+            @endforeach
+        @endif
         <div class="card">
             <div class="card-header d-flex justify-content-between">
                 <h4> Edit Posts</h4>
@@ -9,26 +14,34 @@
                 </div>
             </div>
             <div class="card-body">
-                <form action="" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('posts.update', $post->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
                     <div class="form-group">
+                        <div>
+                            <img src="{{ asset($post->image) }}" alt="" width="200px">
+                        </div>
                         <label for="" class="form-label">Image</label>
-                        <input type="file" class="form-control">
+                        <input type="file" class="form-control" name="image">
                     </div>
                     <div class="form-group">
                         <label for="" class="form-label">Title</label>
-                        <input type="text" class="form-control">
+                        <input type="text" class="form-control" name="title" value="{{ $post->title }}">
                     </div>
                     <div class="form-group">
                         <label for="" class="form-label">Category</label>
-                        <select name="" id="" class="form-control">
-                            <option value="">Test 1</option>
-                            <option value="">Test 2</option>
-                            <option value="">Test 3</option>
+                        <select name="category_id" id="" class="form-control">
+                            <option value="" selected disabled>Select Category</option>
+                            @foreach ($categories as $category)
+                                <option {{ $category->id == $post->category_id ? 'selected' : '' }}
+                                    value="{{ $category->id }}">
+                                    {{ $category->name }}</option>
+                            @endforeach
                         </select>
                     </div>
                     <div class="form-group">
                         <label for="" class="form-label">Description</label>
-                        <textarea name="" id="" class="form-control" cols="30" rows="6"></textarea>
+                        <textarea name="description" id="" class="form-control" cols="30" rows="6">{{ $post->description }}</textarea>
                     </div>
                     <div class="form-group">
                         <button type="submit" class="btn btn-primary">Submit</button>
